8a6cd9e1ced49739b9571ed8c3badc50ec980674
Simon Braillard <simon.braillard@gmail.com>
2020-01-08 21:49:35 +0100

    Adding themes

--- lib/UI/Theme.dart
+++ lib/UI/Theme.dart
@@ -4,6 +4,7 @@ ThemeData easyThemeLight = ThemeData(
   brightness: Brightness.light,
   primaryColor: Color(0xFF3EBACE),
   accentColor: Color(0xFFD8ECF1),
+  cardColor: Colors.white,
   scaffoldBackgroundColor: Color(0xFFF3F5F7),
   primaryTextTheme: TextTheme(
     title: TextStyle(
@@ -27,6 +28,7 @@ ThemeData easyThemeDark = ThemeData(
   brightness: Brightness.dark,
     primaryColor: Color(0xFF828282),
     accentColor: Color(0xFF000000),
+    cardColor: Color(0XFF222324),
     scaffoldBackgroundColor: Color(0xFF1B1B1B),
     primaryTextTheme: TextTheme(
         title: TextStyle(
