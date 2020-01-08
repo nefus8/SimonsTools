d0d3c7d4a81cdd1c5f1fa10cd3f2379a8935d200
Simon Braillard <simon.braillard@gmail.com>
2020-01-08 18:07:49 +0100

    Test

---
+++ lib/widgets/TestWidget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class TestWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Container(
+      child: Text("Hey !"),
+    ));
+  }
+}
