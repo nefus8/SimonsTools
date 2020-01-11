02511f356eacb5f645be4a7dbf6a69125e8f7395
Simon Braillard <simon.braillard@gmail.com>
2020-01-11 18:53:12 +0100

    Responsive size added

---
+++ lib/utils/responsive.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+
+class Responsive {
+  static double getResponsiveSize(double size, BuildContext context) {
+    double shortestSide = MediaQuery.of(context).size.shortestSide;
+    return size * _getRatio(shortestSide);
+  }
+
+  static double _getRatio(double shortestSide) {
+    if (shortestSide <= 650) //iphone 5s
+      return 0.6;
+    if (shortestSide > 650 && shortestSide <= 1200) //iphone X
+      return 1;
+    if (shortestSide > 1200 && shortestSide <= 1530)
+      return 1.275;
+    if (shortestSide > 1530 && shortestSide <= 2048)
+      return 1.7;
+    if (shortestSide > 2048)
+      return 2;
+  }
+}
