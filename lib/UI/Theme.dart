618a3a1d9bee4ceea5645c9736006cd5ca24da7f
Simon Braillard <simon.braillard@gmail.com>
2020-01-12 20:24:37 +0100

    Adjusting

--- lib/UI/Theme.dart
+++ lib/UI/Theme.dart
@@ -65,4 +65,7 @@ ThemeData easyThemeLight2 = ThemeData(
         ),
         body1: TextStyle(
           color: Colors.white,
-        )));
+        )
+    ),
+    buttonColor: Colors.blueAccent,
+);
