b214cb0d03d9ceca5e3df8c159394a318ee89b7d
Simon Braillard <simon.braillard@gmail.com>
2020-01-13 21:17:46 +0100

    Responsive number of columns

--- lib/utils/responsive.dart
+++ lib/utils/responsive.dart
@@ -18,4 +18,14 @@ class Responsive {
     if (shortestSide > 2048)
       return 2;
   }
+
+  static int getResponsiveNumberOfColumns(BuildContext context) {
+    double shortestSide = MediaQuery.of(context).size.shortestSide * MediaQuery.of(context).devicePixelRatio;
+    if (shortestSide <= 650) //iphone 5s
+      return 1;
+    if (shortestSide > 650 && shortestSide <= 1530) //iphone X
+      return 2;
+    if (shortestSide > 1530)
+      return 3;
+  }
 }
