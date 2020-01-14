8a229cdb92a84897f24431f656db758b8c9c1349
Simon Braillard <simon.braillard@gmail.com>
2020-01-14 21:44:13 +0100

    Button animation

--- lib/widgets/textured_tile_button.dart
+++ lib/widgets/textured_tile_button.dart
@@ -67,7 +67,7 @@ class _TexturedTileButtonState extends State<TexturedTileButton> with TickerProv
         child: Stack(
           children: <Widget>[
             Container(
-              height: 12000,
+              height: 800,
               decoration: BoxDecoration(
                 borderRadius: _radius,
                 boxShadow: (widget.boxShadow && _showShadow) ? [BoxShadow(
