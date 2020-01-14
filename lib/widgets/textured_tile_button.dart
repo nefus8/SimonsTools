a2b5316a95743704eb773c4841257ad34f008ff6
Simon Braillard <simon.braillard@gmail.com>
2020-01-14 19:29:25 +0100

    Adding Buttons

---
+++ lib/widgets/textured_tile_button.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TexturedTileButton extends StatefulWidget {
+  final double width, height;
+  final double borderRadius;
+  final String imageUrl;
+  final List<Color> colors;
+  final Alignment begin, end;
+  final double left, bottom;
+  final String text;
+  final TextStyle style;
+  final bool boxShadow;
+
+  TexturedTileButton({
+    @required this.text,
+    @required this.imageUrl,
+    this.width = 200,
+    this.height = 200,
+    this.borderRadius = 30,
+    this.colors = const [Color(0xCBfebea9), Color(0xBBff9d98)],
+    this.begin = Alignment.topRight,
+    this.end = Alignment.bottomLeft,
+    this.left = 20.0,
+    this.bottom = 20.0,
+    this.style = const TextStyle(
+      color: Colors.white,
+      fontWeight: FontWeight.bold,
+      fontSize: 18,
+    ),
+    this.boxShadow = true,
+  });
+
+  @override
+  _TexturedTileButtonState createState() => _TexturedTileButtonState();
+}
+
+class _TexturedTileButtonState extends State<TexturedTileButton> {
+  bool _isEnabled = false;
+
+  @override
+  Widget build(BuildContext context) {
+    BorderRadius _radius = BorderRadius.circular(widget.borderRadius);
+    Image _image;
+
+    if (widget.imageUrl.startsWith("http")) {
+      _image = Image.network(widget.imageUrl, fit: BoxFit.cover);
+    } else {
+      _image = Image.asset(
+        widget.imageUrl,
+        fit: BoxFit.cover,
+      );
+    }
+
+    return GestureDetector(
+      onTapDown: (_) => setState(() {_isEnabled = true;}),
+      onTapUp: (_) => setState(() {_isEnabled = false;}),
+      onTapCancel: () => setState(() {_isEnabled = false;}),
+      child: Stack(
+        children: <Widget>[
+          Container(
+            height: widget.height,
+            width: widget.width,
+            decoration: BoxDecoration(
+              borderRadius: _radius,
+              boxShadow: widget.boxShadow ? [BoxShadow(
+                color: widget.colors[0],
+                offset: Offset(0.0, 13.0),
+                blurRadius: 10.0,
+                spreadRadius: -5.0
+            )]:null,
+            ),
+            child: ClipRRect(
+              borderRadius: _radius,
+              child: _image,
+            ),
+          ),
+          Container(
+            height: widget.height,
+            width: widget.width,
+            decoration: BoxDecoration(
+              borderRadius: _radius,
+              color: widget.colors.length == 1 ? widget.colors[0] : null,
+              gradient: widget.colors.length > 1 ? LinearGradient(
+                colors: widget.colors,
+                begin: widget.begin,
+                end: widget.end,
+              ) : null,
+            ),
+          ),
+          Container(
+            height: widget.height,
+            width: widget.width,
+            decoration: BoxDecoration(
+              borderRadius: _radius,
+              color: _isEnabled ? Color(0x99000000) : null,
+            ),
+          ),
+          Positioned(
+            left: widget.left,
+            bottom: widget.bottom,
+            child: Text(
+              widget.text,
+              style: widget.style,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
