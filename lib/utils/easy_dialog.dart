c5940c3156b917449e67aa5f01fda0b3ac91b878
Simon Braillard <simon.braillard@gmail.com>
2020-01-19 13:42:05 +0100

    Dialog with actions

--- lib/utils/easy_dialog.dart
+++ lib/utils/easy_dialog.dart
@@ -48,4 +48,67 @@ class EasyDialog {
       );
     }
   }
+
+  static void showDialogWithOptions({
+    @required BuildContext context,
+    @required String title,
+    @required String content,
+    @required String textButton1,
+    String textButton2 = 'Annuler',
+    @required Function onPressedButton1,
+    Function onPressedButton2,
+    bool isButton2ACancelButton = true,
+    }){
+    // flutter defined function
+    if (Platform.isIOS) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          // return object of type Dialog
+          return CupertinoAlertDialog(
+            title: Center(child: new Text(title)),
+            content: new Text(content),
+            actions: <Widget>[
+              CupertinoDialogAction(
+                child: Text(textButton1, textAlign: TextAlign.justify,),
+                onPressed: onPressedButton1 != null ? onPressedButton1 : () => Navigator.of(context).pop(),
+              ),
+              CupertinoDialogAction(
+                child: Text(textButton2, textAlign: TextAlign.justify,),
+                onPressed: onPressedButton2 != null ? onPressedButton2 : () => Navigator.of(context).pop(),
+                isDestructiveAction: isButton2ACancelButton,
+                isDefaultAction: true,
+              ),
+            ],
+          );
+        },
+      );
+    } else {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          // return object of type Dialog
+          return AlertDialog(
+            title: Center(child: new Text(title, style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold),)),
+            content: new Text(content, textAlign: TextAlign.justify,),
+            actions: <Widget>[
+              // usually buttons at the bottom of the dialog
+              new FlatButton(
+                child: new Text(textButton1,
+                  style: TextStyle(fontSize: 20, color: Colors.white),),
+                onPressed: onPressedButton1,
+              ),
+              new FlatButton(
+                child: new Text(textButton2,
+                  style: TextStyle(fontSize: 20, color: isButton2ACancelButton ? Colors.redAccent : Colors.white),),
+                onPressed: onPressedButton2 != null ? onPressedButton2 : () => Navigator.of(context).pop(),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
 }
